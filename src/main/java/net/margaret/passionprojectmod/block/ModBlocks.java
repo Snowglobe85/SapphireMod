dce0bc3193e8ecdfe5818d1c0bfe12bd9ab30c0c
Margaret Petersen <margaretelizabethpetersen@gmail.com>
2024-05-27 16:37:49 -0400

    Initial commit

---
+++ src/main/java/net/margaret/passionprojectmod/block/ModBlocks.java
@@ -0,0 +1,66 @@
+package net.margaret.passionprojectmod.block;
+
+import net.margaret.passionprojectmod.PassionProjectMod;
+import net.margaret.passionprojectmod.block.custom.SoundBlock;
+import net.margaret.passionprojectmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, PassionProjectMod.MOD_ID);
+
+    public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerblock("sapphire_block",
+        () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerblock("raw_sapphire_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.RAW_IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    public static final RegistryObject<Block> SAPPHIRE_ORE = registerblock("sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+
+    public static final RegistryObject<Block> DEEPSLATE_SAPPHIRE_ORE = registerblock("deepslate_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE)
+                    .strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+    public static final RegistryObject<Block> NETHER_SAPPHIRE_ORE = registerblock("nether_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERRACK)
+                    .strength(1f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+    public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerblock("end_stone_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
+                    .strength(3f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+    public static final RegistryObject<Block> SOUND_BLOCK = registerblock("sound_block",
+            () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.RAW_IRON_BLOCK)));
+
+
+
+    private static <T extends Block> RegistryObject<T> registerblock(String name, Supplier<T> block){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    };
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+
+    }
+}
---
+++ src/main/java/net/margaret/passionprojectmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,45 @@
+package net.margaret.passionprojectmod.item;
+
+import net.margaret.passionprojectmod.PassionProjectMod;
+import net.margaret.passionprojectmod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PassionProjectMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+        () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                .title(Component.translatable("creativetab.tutorial_tab"))
+                .displayItems((itemDisplayParameters, output) -> {
+                    output.accept(ModItems.SAPPHIRE.get());
+                    output.accept(ModItems.RAW_SAPPHIRE.get());
+
+                    output.accept(ModItems.METAL_DETECTOR.get());
+
+                    output.accept(ModItems.STRAWBERRY.get());
+                    output.accept(ModItems.PINE_CONE.get());
+
+                    output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                    output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                    output.accept(ModBlocks.SAPPHIRE_ORE.get());
+                    output.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                    output.accept(ModBlocks.NETHER_SAPPHIRE_ORE .get());
+                    output.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+                    output.accept(ModBlocks.SOUND_BLOCK.get());
+                })
+                .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
