d9112550b57419bfc6a3d3197239962e3bda3dd8
Margaret Petersen <margaretelizabethpetersen@gmail.com>
2024-05-30 16:25:27 -0400

    Update

--- src/main/java/net/margaret/passionprojectmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/margaret/passionprojectmod/datagen/ModItemModelProvider.java
@@ -33,6 +33,13 @@ public class ModItemModelProvider extends ItemModelProvider {
         buttonItem(ModBlocks.SAPPHIRE_BUTTON, ModBlocks.SAPPHIRE_BLOCK);
         wallItem(ModBlocks.SAPPHIRE_WALL, ModBlocks.SAPPHIRE_BLOCK);
 
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_STAIRS);
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_SLAB);
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_PRESSURE_PLATE);
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_FENCE_GATE);
+
+        trapdoorItem(ModBlocks.SAPPHIRE_TRAPDOOR);
+
         handheldItem(ModItems.SAPPHIRE_SWORD);
         handheldItem(ModItems.SAPPHIRE_AXE);
         handheldItem(ModItems.SAPPHIRE_PICKAXE);
@@ -46,6 +53,17 @@ public class ModItemModelProvider extends ItemModelProvider {
                 new ResourceLocation("item/generated")).texture("layer0",
                 new ResourceLocation(PassionProjectMod.MOD_ID, "item/" + item.getId().getPath()));
     }
+
+    public void evenSimplerBlockItem(RegistryObject<Block> block) {
+        this.withExistingParent(PassionProjectMod.MOD_ID + ":" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath()));
+    }
+
+    public void trapdoorItem(RegistryObject<Block> block) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath() + "_bottom"));
+    }
+
 
     public void fenceItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock){
         this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
